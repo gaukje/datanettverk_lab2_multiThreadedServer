b5ec240f3bd0df4953586a18d549282e602bab1f
gautte <s362066@oslomet.no>
2023-02-08 11:55:40 +0100

    "Finished" servery.py

--- server.py
+++ server.py
@@ -4,3 +4,25 @@ The other side must be passive
 ☞ waits for someone else to take initiative for creating a connection
 ☞ this side is called the server
 """
+from socket import *
+import _thread as thread
+import time
+
+def now():
+    #Returns the time of day
+    return time.ctime(time.time())
+
+def handleClient(connection):
+    #Client handler
+    while True:
+        data = connection.recv(1024).decode()
+        
+        print("received message = ", data)
+        
+        modified_message = data.upper()
+        connection.send(modified_message.encode())
+        
+        if(data == "exit"):
+            break
+
+        connection.close()
