ec0790be4447562317519a3dcd0f304ae13c99de
gautte <s362066@oslomet.no>
2023-02-08 15:20:46 +0100

    Changed syntax-errors

--- server.py
+++ server.py
@@ -5,14 +5,14 @@ The other side must be passive
 ☞ this side is called the server
 """
 from socket import *
-import _thread as thread
+import _thread as thread #The import _thread as thread statement imports the thread module as thread, which provides a way to run multiple threads (also known as light-weight processes) in parallel within a single process.
 import time
 
 def now():
     #Returns the time of day
     return time.ctime(time.time())
 
-def handleClient(connection):
+def handleClient(connection):     #Function tgat handles each client connection
     #Client handler
     while True:
         data = connection.recv(1024).decode()
@@ -25,9 +25,15 @@ def handleClient(connection):
         if(data == "exit"):
             break
 
-        connection.close()
+    connection.close()
 
-
+"""
+handleClient(connection) is the function that handles each client connection. It reads data from the client using 
+connection.recv(1024) (the 1024 specifies the maximum number of bytes to be received at once), converts the received 
+bytes to a string using .decode(), and then sends back the modified data (converted to uppercase) using connection.
+send(modified_message.encode()). If the client sends the message "exit", the function breaks out of the while loop and 
+closes the connection using connection.close().
+"""
 def main():
     #Creates a server cocket, listens for new connections and spawns a new thread for new connections
 
@@ -35,7 +41,7 @@ def main():
     serverSocket = socket(AF_INET, SOCK_STREAM)
 
     try:
-        serverSocket.bind('',serverPort)
+        serverSocket.bind(('',serverPort))
     
     except: 
         print("Bind failed. Error :" )
@@ -48,6 +54,7 @@ def main():
         print('Server connected by ', addr)
         print('at ', now())
         thread.start_new_thread(handleClient, (connectionSocket,))
+    
     serverSocket.close()
 
 if __name__ == '__main__':
